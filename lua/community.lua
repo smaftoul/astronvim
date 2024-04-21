69bdbc09bead00c0b4a5925765c8cbeb8fd54ecd
Samuel Maftoul <samuel.maftoul@gmail.com>
2024-04-21 17:48:02 +0200

    feat: init own config

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,13 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
 }
